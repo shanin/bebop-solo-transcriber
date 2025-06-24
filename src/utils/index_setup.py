02f20c676e40e0462847ffd1923b96e43d2d47e1
Ivan Shanin <ivan.shanin@gmail.com>
2025-06-24 14:23:42 +0100

    [is] -- added indexes for augmented flosax samples

--- src/utils/index_setup.py
+++ src/utils/index_setup.py
@@ -6,12 +6,13 @@ import os
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str, choices=['filosax', 'omnibook', 'both'], default='both')
+    parser.add_argument('--dataset', type=str, choices=['filosax', 'uvr_filosax', 'omnibook', 'all'], default='all')
     parser.add_argument('--filosax_path', type=str, required=True)
+    parser.add_argument('--uvr_filosax_path', type=str, required=True)
     parser.add_argument('--omnibook_path', type=str, required=True)
     args = parser.parse_args()
 
-    if args.dataset in ['filosax', 'both']:
+    if args.dataset in ['filosax', 'all']:
         lines = []
         # Filosax
         for participant in range(1, 6):
@@ -30,7 +31,27 @@ if __name__ == '__main__':
         df = pd.DataFrame(lines)
         df.to_csv('stages/1_processed_scores/index_filosax.csv', index=False)
 
-    if args.dataset in ['omnibook', 'both']:
+    if args.dataset in ['uvr_filosax', 'all']:
+        lines = []
+        suffix = '_(Woodwinds)_17_HP-Wind_Inst-UVR.wav'
+        for relative_sax_loudness in [2, 5, 8]:
+            for participant in range(1, 6):
+                for song in range(1, 49):
+                    id_ = f'FS{participant}_{song:02d}'
+                    lines.append({
+                        'example_id': id_,
+                        'participant': participant,
+                        'song': song,
+                        'dataset': 'filosax',
+                        'clean_solo': f'{args.uvr_filosax_path}/{id_}_L{relative_sax_loudness}{suffix}',
+                        'backing_pd': np.nan,
+                        'backing_bd': np.nan,
+                        'mix_path': np.nan,
+                    })
+            df = pd.DataFrame(lines)
+            df.to_csv(f'stages/1_processed_scores/index_uvr_filosax_L{relative_sax_loudness}.csv', index=False)
+
+    if args.dataset in ['omnibook', 'all']:
         lines = []
         for song_file in os.listdir(f'{args.omnibook_path}/audio_stems'):
             if song_file.endswith('.wav'):
