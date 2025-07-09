d30c0ce1ae58f48cc51be5d9cab85407c058c31c
Ivan Shanin <ivan.shanin@gmail.com>
2025-07-09 10:26:51 +0100

    [is] -- dataset

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -9,32 +9,6 @@ from typing import Iterator, List
 import numpy as np
 from functools import partial
 
-def int_to_rhythm_str(k: int) -> str:
-    integer = k
-    result = 'oooooooooooo'
-    if integer != 0:
-        substitution = {0: 'o', 1: 't', 2: 'r', 3: 'x'}
-        digits = []
-        while integer:
-            digits.append(int(integer % 4))
-            integer //= 4
-        str_end = ''.join([substitution[digit] for digit in digits[::-1]])
-        result = result[:-len(str_end)] + str_end
-    return result
-
-def int_to_mask(k: int) -> List[int]:
-    integer = k
-    result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    if integer != 0:
-        digits = []
-        while integer:
-            digits.append(int(integer % 4))
-            integer //= 4
-        end = digits[::-1]
-        for i in range(len(end)):
-            result[-len(end) + i] = end[i]
-    return result
-
 class TrackDataset(Dataset):
     def __init__(self, data_dir=None, source: str = 'original', split: str = 'all'):
         self.data_dir = data_dir
@@ -45,24 +19,11 @@ class TrackDataset(Dataset):
         if self.split != 'all':
             self.prepare_splits()
         self.prepare_file_list()
-        self.load_songs()
-
 
     def process_annotations(self, data):
         data['scalar_features'] = torch.nan_to_num(data['scalar_features'], nan=0.0, posinf=1.0, neginf=0.0)
         data['activations'] = torch.nan_to_num(data['activations'], nan=0.0, posinf=1.0, neginf=0.0)
         return data
-
-
-    def load_songs(self):
-        self.songs = []
-        # Load all data into memory
-        print(f"Loading {len(self.files)} files from {self.data_dir}, mode: {self.source}, split: {self.split}")
-        for file in tqdm(self.files):
-            file_path = os.path.join(self.data_dir, file)
-            data = torch.load(file_path)
-            data = self.process_annotations(data)
-            self.songs.append(data)
 
     def prepare_file_list(self):
         if self.split == 'train':
@@ -75,105 +36,22 @@ class TrackDataset(Dataset):
             self.files = self.all_files
         else:
             assert False, f"Invalid split: {self.split}"
-        
-    def generate_rhythm_token(self, integer: int):
-        signature = int_to_rhythm_str(integer)
-        if signature in self.rhythm_tokens:
-            return self.rhythm_tokens[signature]['id']
-        elif 'x' in signature:
-            return 42 # too fast rhythm
-        else:
-            return 43 # too rare rhythm
-        
-    def generate_inferred_time_feel(self, integer):
-        signature = int_to_rhythm_str(integer)
-        if signature in self.rhythm_tokens:
-            if self.rhythm_tokens[signature]['feel'] == 'swing':
-                return 0
-            elif self.rhythm_tokens[signature]['feel'] == 'double_time':
-                return 1
-            else:
-                assert False
-        elif 'x' in signature:
-            return 1
-        else:
-            return 1 # too rare rhythm 
-
-    def generate_mask(self, integer: int):
-        mask = int_to_mask(integer)
-        return torch.tensor(mask, dtype=torch.int64)
 
     def __len__(self):
         return len(self.songs)
-    
-    #def fix_tokens(self, tokens: torch.Tensor):
-    #    # due to a preprocessing bug there is a meaningless token 128
-    #    tokens[tokens == 129] = 128 # tie token
-   #     tokens[tokens == 130] = 129 # rest token
-   #     return tokens
 
     def __getitem__(self, idx):
-        return self.songs[idx]
-        bar = self.data[idx]
-        flux = torch.tensor(bar['features']['flux']).view(48)
-        confidence = torch.tensor(bar['features']['confidence']).view(48)
-        amplitude = torch.tensor(bar['features']['amplitude']).view(48)
-        activations = torch.tensor(bar['features']['activations']).view(48, 384)
-        
-        # Filter out NaN values in features
-        flux = torch.nan_to_num(flux, nan=0.0, posinf=1.0, neginf=0.0)
-        confidence = torch.nan_to_num(confidence, nan=0.0, posinf=1.0, neginf=0.0)
-        amplitude = torch.nan_to_num(amplitude, nan=0.0, posinf=1.0, neginf=0.0)
-        
-        # Filter out NaN values in activations
-        activations = torch.nan_to_num(activations, nan=0.0, posinf=1.0, neginf=0.0)
-        
-        features = torch.stack([flux, confidence, amplitude], dim = -1)
-
-        tokens = torch.concatenate([
-            torch.tensor(bar['annotation'][0]['beatwise_score']),
-            torch.tensor(bar['annotation'][1]['beatwise_score']),
-            torch.tensor(bar['annotation'][2]['beatwise_score']),
-            torch.tensor(bar['annotation'][3]['beatwise_score']),
-        ])
-        tokens = self.fix_tokens(tokens)
-
-        mask = torch.concatenate([
-            self.generate_mask(bar['annotation'][0]['rhythm_signature']),
-            self.generate_mask(bar['annotation'][1]['rhythm_signature']),
-            self.generate_mask(bar['annotation'][2]['rhythm_signature']),
-            self.generate_mask(bar['annotation'][3]['rhythm_signature']),
-        ])
-
-        rhythm_tokens = torch.tensor([
-            self.generate_rhythm_token(bar['annotation'][0]['rhythm_signature']),
-            self.generate_rhythm_token(bar['annotation'][1]['rhythm_signature']),
-            self.generate_rhythm_token(bar['annotation'][2]['rhythm_signature']),
-            self.generate_rhythm_token(bar['annotation'][3]['rhythm_signature']),
-        ])
-
-        inferred_time_feel = torch.tensor([
-            self.generate_inferred_time_feel(bar['annotation'][0]['rhythm_signature']),
-            self.generate_inferred_time_feel(bar['annotation'][1]['rhythm_signature']),
-            self.generate_inferred_time_feel(bar['annotation'][2]['rhythm_signature']),
-            self.generate_inferred_time_feel(bar['annotation'][3]['rhythm_signature']),
-        ])
-
-        source_time_feel = torch.tensor(self.source != 'original', dtype=torch.int64)
-
-
-        return {
-            'features': features,
-            'activations': activations,
-            'tokens': tokens,
-            'mask': mask,
-            'rhythm_tokens': rhythm_tokens,
-            'inferred_time_feel': inferred_time_feel,
-            'source_time_feel': source_time_feel,
-        }
+        file_path = os.path.join(self.data_dir, self.files[idx])
+        data = torch.load(file_path)
+        data = self.process_annotations(data)
+        return data
     
 
 class OmnibookDataset(TrackDataset):
+    def __init__(self, data_dir=None, source: str = 'original', split: str = 'all'):
+        super().__init__(data_dir, source, split)
+        self.instrument = 'alto'
+
     def prepare_splits(self):
         self.test_files = [
             'OB_1p64c.original.pt', 'OB_S5VYc.original.pt', 'OB_wkTyc.original.pt',
@@ -185,12 +63,26 @@ class OmnibookDataset(TrackDataset):
 
 
 class FilosaxDataset(TrackDataset):
+    def __init__(self, data_dir=None, source: str = 'original', split: str = 'all'):
+        super().__init__(data_dir, source, split)
+        self.instrument = 'tenor'
+
     def prepare_splits(self):
         self.test_files = [f'FS{i}_46.{self.source}.pt' for i in range(1, 6)] + \
                     [f'FS{i}_47.{self.source}.pt' for i in range(1, 6)] + \
                     [f'FS{i}_48.{self.source}.pt' for i in range(1, 6)]
         self.val_files = [f'FS{i}_45.{self.source}.pt' for i in range(1, 6)]
         self.train_files = [f for f in self.all_files if f not in self.test_files and f not in self.val_files and f.endswith(f'.{self.source}.pt')]
+
+"""
+class SegmentDataset(Dataset):
+    def __init__(self, dataset, num_consecutive_bars: int, random_transposition: bool = False):
+        self.dataset = dataset
+        self.num_consecutive_bars = num_consecutive_bars
+        self.random_transposition = random_transposition
+"""
+    
+
 
 class DEPRECATED_ConsecutiveBarSampler(Sampler):
     def __init__(self, dataset, num_consecutive_bars: int, batch_size: int, inference: bool = False):
