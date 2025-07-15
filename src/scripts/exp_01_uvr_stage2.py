ca5341501f5cd24208604f63d37891a076e68838
Ivan Shanin <ivan.shanin@gmail.com>
2025-07-15 18:27:09 +0100

    [is] -- script bugfix

--- src/scripts/exp_01_uvr_stage2.py
+++ src/scripts/exp_01_uvr_stage2.py
@@ -53,17 +53,7 @@ def main(args):
         sampler=sampler
     )
 
-    model = RhythmScaffoldLightningModule(
-        embedding_dim=128,
-        num_heads=4,
-        num_layers=4,
-        teacher_forcing=True,
-        project_name="solo-transcriber",
-        experiment_name = args.experiment_name
-    )
-
-    if args.starting_checkpoint is not None:
-        model.load_from_checkpoint(args.starting_checkpoint)
+    model = RhythmScaffoldLightningModule.load_from_checkpoint(args.starting_checkpoint)
 
     early_stop_callback = EarlyStopping(
         monitor="val_loss",     # or any other metric you log
