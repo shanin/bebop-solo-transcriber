688eb1f549478b4902a5f377f3eba3edbad46611
Ivan Shanin <ivan.shanin@gmail.com>
2025-08-07 16:13:44 +0100

    [is] -- command for model b experiment

---
+++ src/scripts/exp_07_model_b.py
@@ -0,0 +1,75 @@
+from src.dataset.dataset import FilosaxDataset, SegmentDataset
+from torch.utils.data import DataLoader
+from src.model.model_b import RhythmScaffoldLightningModule
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+import argparse
+
+def main(args):
+    filosax_train = FilosaxDataset(data_dir=args.filosax_dir, split = 'train', source = 'original')
+    filosax_train_segments = SegmentDataset(filosax_train, num_consecutive_bars = 8, random_transposition = args.transpose_augmentation)
+    filosax_train_loader = DataLoader(filosax_train_segments, batch_size=512, shuffle=True, num_workers=0, pin_memory=True)
+
+    filosax_val = FilosaxDataset(data_dir=args.filosax_dir, split = 'val', source = 'original')
+    filosax_val_segments = SegmentDataset(filosax_val, num_consecutive_bars = 8, random_transposition = False)
+    filosax_val_loader = DataLoader(filosax_val_segments, batch_size=512, shuffle=False, num_workers=0)
+
+    filosax_test = FilosaxDataset(data_dir=args.filosax_dir, split = 'test', source = 'original')
+    filosax_test_segments = SegmentDataset(filosax_test, num_consecutive_bars = 8, random_transposition = False)
+    filosax_test_loader = DataLoader(filosax_test_segments, batch_size=512, shuffle=False, num_workers=0)
+
+    model = RhythmScaffoldLightningModule(
+        embedding_dim=args.embedding_dim,
+        num_heads=args.num_heads,
+        num_layers=args.num_layers,
+        num_backend_heads=args.num_backend_heads,
+        num_backend_layers=args.num_backend_layers,
+        teacher_forcing=True,
+        project_name="solo-transcriber",
+        experiment_name = args.experiment_name,
+        rhythm_loss_weight = args.rhythm_loss_weight
+    )
+
+    early_stop_callback = EarlyStopping(
+        monitor="val_loss",     # or any other metric you log
+        patience=5,             # how many epochs with no improvement to wait
+        mode="min",             # "min" for loss, "max" for accuracy, etc.
+        verbose=True
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_loss",
+        save_top_k=1,
+        mode="min",
+        filename="best-model"
+    )
+
+    trainer = pl.Trainer(
+        max_epochs=100,
+        logger=model.wandb_logger,
+        accelerator='gpu' if torch.cuda.is_available() else 'cpu',
+        callbacks=[early_stop_callback, checkpoint_callback]
+    )
+
+    trainer.fit(model, filosax_train_loader, filosax_val_loader)
+
+    trainer.test(model, filosax_test_loader)
+
+
+    print(checkpoint_callback.best_model_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--experiment_name", type=str, default="default")
+    parser.add_argument("--filosax_dir", type=str, required=True)
+    parser.add_argument("--transpose_augmentation", type=bool, default=True)
+    parser.add_argument("--rhythm_loss_weight", type=float, default=1.0)
+    parser.add_argument("--num_layers", type=int, default=4)
+    parser.add_argument("--embedding_dim", type=int, default=128)
+    parser.add_argument("--num_heads", type=int, default=4)
+    parser.add_argument("--num_backend_heads", type=int, default=4)
+    parser.add_argument("--num_backend_layers", type=int, default=2)
+    args = parser.parse_args()
+    main(args)
