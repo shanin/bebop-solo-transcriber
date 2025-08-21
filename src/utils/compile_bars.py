f801bb8d64b6b3e35147eb88879de7e75dd3cf85
Ivan Shanin <ivan.shanin@gmail.com>
2025-08-21 15:45:47 +0100

    [is] -- removed audio features from bars

--- src/utils/compile_bars.py
+++ src/utils/compile_bars.py
@@ -13,36 +13,6 @@ from src.tokenizer.rhythm_utils import (
 )
 
 
-def prepare_bar(onsets, offsets, frames, posenc, bar_num=0):
-
-    bar_index = posenc[:, 0]
-    beat_index = posenc[:, 1]
-
-    bar_onsets = []
-    bar_offsets = []
-    bar_frames = []
-    bar_posenc = []
-
-    for beat_idx in range(4):
-
-        beat_onsets = onsets[(beat_index == beat_idx) & (bar_index == bar_num)]
-        beat_offsets = offsets[(beat_index == beat_idx) & (bar_index == bar_num)]
-        beat_frames = frames[(beat_index == beat_idx) & (bar_index == bar_num)]
-        beat_posenc = posenc[(beat_index == beat_idx) & (bar_index == bar_num)]
-
-        bar_onsets.append(beat_onsets.tolist())
-        bar_offsets.append(beat_offsets.tolist())
-        bar_frames.append(beat_frames.tolist())
-        bar_posenc.append(beat_posenc.tolist())
-    
-    return {
-        'onsets': bar_onsets,
-        'offsets': bar_offsets,
-        'frames': bar_frames,
-        'posenc': bar_posenc,
-    }
-
-
 def fix_score(score):
     score = np.array(score)
     score[score == 129] = 128 # tie token
@@ -64,60 +34,34 @@ def combine_filosax(labeled_scores, crnn_folder, posenc_folder, output_folder):
         for song in range(1, 49):
             print(f'Processing {participant} {song}...')
             fsid = f'FS{participant}_{song:02d}'
-
-            onsets = np.load(os.path.join(crnn_folder, f'{fsid}.onsets.npy'))
-            offsets = np.load(os.path.join(crnn_folder, f'{fsid}.offsets.npy'))
-            frames = np.load(os.path.join(crnn_folder, f'{fsid}.frames.npy'))
-            posenc = np.load(os.path.join(posenc_folder, f'{fsid}.posenc.npy'))
             
             metadata = labeled_scores[labeled_scores['participant'] == participant]
             metadata = metadata[metadata['song'] == song]
 
-            song_onsets = []
-            song_offsets = []
-            song_frames = []
-            song_posenc = []
             song_score_annotations = []
             song_rhythm_signature = []
             song_data_flags = []
             song_raw_rhythm_signature = []
             for _, row in metadata.iterrows():
                 if row['double_time'] is False:
-                    bar_features = prepare_bar(onsets, offsets, frames, posenc, bar_num=row['bar_num'])
                     bar_annotation = prepare_annotations(row)
-                    song_onsets.append(bar_features['onsets'])
-                    song_offsets.append(bar_features['offsets'])
-                    song_frames.append(bar_features['frames'])
-                    song_posenc.append(bar_features['posenc'])
                     song_score_annotations.append(bar_annotation['beatwise_score'])
                     song_rhythm_signature.append(bar_annotation['rhythm_signature'])
                     song_data_flags.append(bar_annotation['flags'])
                     song_raw_rhythm_signature.append(bar_annotation['raw_rhythm_signature'])
 
             # Convert lists to tensors
-            onsets_tensor = torch.tensor(song_onsets)
-            offsets_tensor = torch.tensor(song_offsets)
-            frames_tensor = torch.tensor(song_frames)
-            posenc_tensor = torch.tensor(song_posenc)
             score_annotations_tensor = torch.tensor(song_score_annotations)
             rhythm_signature_tensor = torch.tensor(song_rhythm_signature)
             data_flags_tensor = torch.tensor(song_data_flags)
 
             # Save tensors in a dictionary
             torch_data = {
-                'onsets': onsets_tensor,
-                'offsets': offsets_tensor,
-                'frames': frames_tensor,
-                'posenc': posenc_tensor,
                 'tokens': score_annotations_tensor,
                 'rhythm_signatures': rhythm_signature_tensor,
                 'flags': data_flags_tensor
             }
 
-            torch_data['onsets'] = torch_data['onsets'].transpose(1, 2)
-            torch_data['offsets'] = torch_data['offsets'].transpose(1, 2)
-            torch_data['frames'] = torch_data['frames'].transpose(1, 2)
-            torch_data['posenc'] = torch_data['posenc'].transpose(1, 2)
             torch_data['rhythm_tokens'] = torch.stack([torch.tensor([generate_rhythm_token(x) for x in bar]) for bar in song_raw_rhythm_signature])
             torch_data['inferred_time_feel'] = torch.stack([torch.tensor([generate_inferred_time_feel(x) for x in bar]) for bar in song_raw_rhythm_signature])
             torch_data['source_time_feel'] = torch.tensor(True, dtype=torch.int64)
@@ -131,8 +75,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--labeled_scores', type=str, default='../stages/1_processed_scores/labeled_scores.json')
-    parser.add_argument('--crnn_folder', type=str, default='../test')
-    parser.add_argument('--posenc_folder', type=str, default='../test')
     parser.add_argument('--output_folder', type=str, default='../stages/1c_combined_data')
     args = parser.parse_args()
 
