4b19d3d87195f24ab81b1be3ce20cd192f951721
Ivan Shanin <ivan.shanin@gmail.com>
2025-08-29 00:34:06 +0100

    [is] -- debug

--- src/scripts/rhythm_perceiver_inference.py
+++ src/scripts/rhythm_perceiver_inference.py
@@ -173,10 +173,10 @@ def backend_inference(args, model):
     beats = beat_tracking_inference(args)
     posenc = generate_posenc(frames, beats)
     track = {
-        'onsets': torch.from_numpy(onsets).float().to(device),
-        'offsets': torch.from_numpy(offsets).float().to(device), 
-        'frames': torch.from_numpy(frames).float().to(device),
-        'posenc': torch.from_numpy(posenc).float().to(device)
+        'onsets': torch.from_numpy(onsets).float(),
+        'offsets': torch.from_numpy(offsets).float(), 
+        'frames': torch.from_numpy(frames).float(),
+        'posenc': torch.from_numpy(posenc).float()
     }
     segments = BackendSegmentInferenceDataset(track, num_consecutive_bars = args.num_consecutive_bars, use_cache = False)
     loader = DataLoader(segments, batch_size=args.batch_size, shuffle=False, num_workers=0, collate_fn=backend_inference_segment_collate_fn)
